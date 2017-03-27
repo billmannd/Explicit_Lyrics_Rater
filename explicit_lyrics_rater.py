3e745b8da33ffad0420dfc9e0cb540c654e17332
billmannd <billmannd@gmail.com>
2017-03-27 17:08:28 -0400

    Initial commit

---
+++ explicit_lyrics_rater.py
@@ -0,0 +1,12 @@
+import re
+import pandas as pd
+import numpy as np
+import json
+from collections import Counter
+#import requests
+#from bs4 import BeautifulSoup as bs
+#from urllib.request import urlopen
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pickle
+%matplotlib inline
